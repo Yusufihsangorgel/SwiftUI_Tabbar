671eb6e1344af32411ecc9d2ed30207cd67b478c
Yusufihsangorgel <developeryusuf@icloud.com>
2022-07-05 21:07:39 +0300

    Initial Commit

---
+++ 5Temmuz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  5Temmuz
+//
+//  Created by Yusuf İhsan Görgel on 5.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 5Temmuz/_TemmuzApp.swift
@@ -0,0 +1,17 @@
+//
+//  _TemmuzApp.swift
+//  5Temmuz
+//
+//  Created by Yusuf İhsan Görgel on 5.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct _TemmuzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
